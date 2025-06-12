cd21e106396aab1b0aa29cd75511d21cad5d8bae
fjordceo <fjordceo@hotmail.com>
2025-06-12 10:58:01 +0700

    Выполнены эпизоды 16 и 17, а так же домашнее задание

---
+++ Episode016/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Episode016
+{
+    /*
+     * Операторы отношений и операции сравнения в C#.
+     * 
+     * Всего пройдено 15 уроков. Время обучения: 02:45:24
+     * 
+     */
+
+/*
+ *    == равно
+ *    != не равно
+ *    < меньше
+ *    > больше
+ *    <= меньше или равно
+ *    >= больше или равно
+ *    
+ */
+
+    internal class Program
+    {
+         static void Main(string[] args)
+    {
+            int a = 5;
+            int b = 10;
+
+            bool result = a == b;
+
+
+            Console.WriteLine("A равна B?");
+            Console.WriteLine(result);
+            Console.WriteLine("A не равна B?");
+            Console.WriteLine(a!=b);
+            Console.WriteLine("A меньше B");
+            Console.WriteLine(a < b);
+            Console.WriteLine("A больше B");
+            Console.WriteLine(a > b);
+            Console.WriteLine("A меньше или равно B");
+            Console.WriteLine(a <= b);
+            Console.WriteLine("A больше или равно B");
+            Console.WriteLine(a >= b);
+
+
+
+        }
+    }
+}
---
+++ HomeWork002/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace HomeWork002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+
+            Console.WriteLine("Домашнее задание. Проверка числа на четность");
+            Console.Write("Введите целое число: ");
+            if (!int.TryParse(Console.ReadLine(), out value))
+            {
+                Console.WriteLine("Ошибка: введено не целое число. Я же просил!");
+                return;
+            }
+            if (value % 2 == 0)
+            {
+                Console.WriteLine($"Число {value} является четным.");
+            }
+            else
+            {
+                Console.WriteLine($"Число {value} является нечетным.");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
