c4fe11f429dc2c6e23c29d7d81e4a1436d4995c5
fjordceo <fjordceo@hotmail.com>
2025-06-16 12:29:38 +0700

    Пройден урок 025

---
+++ Episode025/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Episode025;
+/*
+ *  Цикл for. Прокаченный while 
+ */
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество циклов");
+        int limit = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i <= limit; i++) // переменная i существует только в рамках данного цикла
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
