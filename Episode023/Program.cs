7f565bc924c7e6cb8a1dd9047f94c7c2a8df634a
fjordceo <fjordceo@hotmail.com>
2025-06-16 11:29:29 +0700

    Закончено домашнее задание 004

--- Episode023/Program.cs
+++ Episode023/Program.cs
@@ -1,7 +1,7 @@
 ﻿namespace Episode023
 
 /*
-* Цикл dowhile в С#
+* Цикл do while в С#
 * 
 * Всего пройдено 22 урока. Время обучения: 04:26:28
 * 
@@ -12,6 +12,20 @@
     {
         static void Main(string[] args)
         {
+            int count = 0;
+            int limit = 500000000;
+
+            do
+            {
+                count++;
+                Console.WriteLine(count);
+            } while (count < limit);
+            
+            
+            
+            
+            
+            
             Console.WriteLine("Hello, World!");
         }
     }
--- HomeWork004/Program.cs
+++ HomeWork004/Program.cs
@@ -1,17 +1,25 @@
 ﻿namespace HomeWork004
 {
 
-    // Домашнее задание. Программа, которая будет подсчитывать количество четных и нечетных чисел в том диапозоне , который введет пользователь.
+    // Домашнее задание. Программа, которая будет подсчитывать количество четных и нечетных чисел. Диапазон вводит пользователь.
     internal class Program
     {
         static void Main(string[] args)
         {
             int count = 0;
-            int limit = 0;
+            int limit;
             int evenCount = 0;
             int oddCount = 0;
 
-
+            Console.WriteLine("Добро пожаловать в программу которая считает количество четных и нечетных чисел выбранного диапазона");
+            Console.WriteLine("Введите число с которого мы начнем отчет");
+            if (!int.TryParse(Console.ReadLine(), out count))
+            {
+                Console.WriteLine("Ошибка. Введено некорректное число!");
+                return;
+            }
+            
+            
             Console.WriteLine("Введите лимит для подсчета четных и нечетных чисел");
             if (!int.TryParse(Console.ReadLine(), out limit))
             {
@@ -20,9 +28,9 @@
             }
 
 
-            while (count < limit)
+            while (count <= limit)
             {
-                count++;
+                
                 if (count % 2 == 0)
                 {
                     evenCount++;
@@ -31,7 +39,8 @@
                 {
                     oddCount++;
                 }
-
+                
+                count++;
             }
 
             Console.WriteLine($"Количество четных чисел: {evenCount}");
