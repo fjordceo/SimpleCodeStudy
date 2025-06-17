e1dee721e44d18163551df57d5c48aec2d5eeca3
fjordceo <fjordceo@hotmail.com>
2025-06-17 15:08:40 +0700

    Пройден урок 029

---
+++ Episode029/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace Episode029;
+
+/*
+ * Вложенные циклы (вложенные конструкции). Используются, например, в двумерных или трехмерных массивах
+ * Работа с вложеннымии данными в ООП
+ */
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine("Цикл №1. Итерация " + i);
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine("\tЦикл №2. Итерация " + j);
+            }
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Введите высоту прямоугольника: ");
+        int height = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите ширину прямоугольника: ");
+        int width = int.Parse(Console.ReadLine());
+        
+        for (int a = 0; a < height; a++)
+        {
+            for (int b = 0; b < width; b++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine();
+        }
+    }
+}
