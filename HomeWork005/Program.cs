a28bd7e0c59e0ea899fd412fd0cbb06c50bdf902
fjordceo <fjordceo@hotmail.com>
2025-06-17 16:01:56 +0700

    Выполнено домашнее задание 005

---
+++ HomeWork005/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace HomeWork005;
+
+// Программа выводящая на экран консоли треугольники, состоящие из #
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char sharp = '#';
+        char space = ' ';
+
+        for (int i = 1; i < 10; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(sharp);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        
+        for (int i = 10; i > 0; i--)
+        {
+            for (int j = 1; j < i; j++)
+            {
+                Console.Write(sharp);
+            }
+            Console.WriteLine();
+        }
+        
+        Console.WriteLine();
+
+        for (int i = 1; i < 10; i++)
+        {
+            for (int j = 0; j < 10 - i; j++)
+            {
+                Console.Write(space);
+            }
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(sharp);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        for (int i = 10; i > 0; i--)
+        {
+            for (int j = 0; j < 10 - i; j++)
+            {
+                Console.Write(space);
+            }
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(sharp);
+            }
+            Console.WriteLine();
+        }
+
+
+
+
+
+
+
+
+    }
+}
