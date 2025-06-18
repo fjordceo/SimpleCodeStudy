bc015acfd4a07ddf0bae36e4c7fa74fcefc5825e
fjordceo <fjordceo@hotmail.com>
2025-06-18 14:17:04 +0700

    Пройден урок 036

---
+++ Episode032/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Episode032;
+/*
+ * Массивы
+ *
+ * Одномерные массивы
+ *
+ * Тип_элементов_массива [] имя_массива
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[] myArray;
+        myArray = new int[5];
+        myArray[0] = 5;
+        myArray[1] = 10;
+        
+        Console.WriteLine(myArray.Length);
+    }
+}
---
+++ Episode033/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Episode033;
+/*
+ * Массивы
+ *
+ * Одномерные массивы
+ *
+ * Способы инициализациии массивов
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Способы инициализации массивов
+        int[] myArray = new int[5]; 
+        //Так же можно не указывать количество элементов массива
+        int[] myArrey1 = new int[5] { 5, 6, 9, 8, 7 }; 
+        //Через Enumerable. Записывает 10 элементов со значением 5
+        int[] myArrey2 = Enumerable.Repeat(5, 10).ToArray();
+        //Через Enumerable.Range. Создает 10 элементов со значением, начинающимся от 10.
+        int[] myArrey3 = Enumerable.Range(10, 10).ToArray();
+        
+        
+
+
+        Console.WriteLine();
+
+    }
+}
