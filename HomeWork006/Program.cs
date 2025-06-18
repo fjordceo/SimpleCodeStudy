199d0fa1319bd2158e376240ecb4e17e4220bdf2
fjordceo <fjordceo@hotmail.com>
2025-06-18 14:47:33 +0700

    Выполнено домашнее задание 006

--- HomeWork006/Program.cs
+++ HomeWork006/Program.cs
@@ -11,6 +11,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int quantity;
+        int sum = 0;
+
+        Console.Write("Пожалуйста, введите количество элементов массива\t");
+        quantity = int.Parse(Console.ReadLine());
+        
+        int[] myArray = new int[quantity];
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            Console.Write($"Введите элемент массива с индексом {i}\t");
+            myArray[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("А теперь выведем основные показатели данного массива. Нажмите любую клавишу");
+        Console.ReadKey();
+
+        Console.Write("Массив в обратном порядке\t");
+        for (int i = myArray.Length - 1; i >= 0; i--)
+        {
+            Console.Write(myArray[i]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            if (myArray[i] % 2 == 0)
+            {
+                sum += myArray[i];
+            }
+        }
+
+        Console.WriteLine($"Сумма четных элементов массива:\t {sum}");
+        
+       int min = myArray[0];
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            if (myArray[i] < min)
+            {
+                min = myArray[i];
+            }
+        }
+        
+        Console.WriteLine($"Наименьший элемент массива:\t {min}");
     }
 }
