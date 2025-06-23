3f6468c6410a66e68245460d44798aec24bdda7f
fjordceo <fjordceo@hotmail.com>
2025-06-23 12:11:50 +0700

    Пройден урок 050

---
+++ Episode048/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Episode048;
+
+/*
+ * Перегрузка методов в C#
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int result = Sum(10, 15); // Вызов перегруженного метода Sum с двумя параметрами
+        Console.WriteLine ($"Результат сложения двух чисел: {result}");
+        result = Sum(10, 15, 20); // Вызов перегруженного метода Sum с тремя параметрами
+        Console.WriteLine ($"Результат сложения трех чисел: {result}");
+        double resultDouble = Sum(10.5, 15.5); // Вызов перегруженного метода Sum с двумя параметрами типа double
+        Console.WriteLine($"Результат сложения двух чисел типа double: {resultDouble}");
+    }
+
+    /// <summary>
+    /// Возвращает сумму трех целых чисел.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <param name="c"></param>
+    /// <returns></returns>
+    static int Sum(int a, int b, int c) 
+    {
+        return a + b + c;
+    }
+
+    /// <summary>
+    /// Возвращает сумму двух целых чисел.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    static int Sum(int a, int b)
+    {
+        return a + b;
+    }
+    
+    /// <summary>
+    /// Возвращает сумму двух чисел типа double.
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    static double Sum (double a, double b) // Перегрузка метода Sum c использованием типа double
+    {
+        return a + b;
+    }
+}
---
+++ HomeWork007/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace HomeWork007;
+
+/*
+ * 1. Написать метод, который выводит на экран строку.
+ *    Символы, из которых состоит строка, и их количество вводятся пользователем.
+ *
+ * 2. Написать метод для поиска индекса элемента массива (тип элементов в массиве - int),
+ *    Метод должен вернуть индекс первого найденного элемента, если он будет найден.
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numberToFind;
+        char symbol;
+        int count;
+
+        int[] myArrey = GetRandomArray(15, 1, 100);
+        
+        
+        Console.WriteLine("Введите символ, из которого будет состоять строка:");
+        symbol = Console.ReadKey().KeyChar;
+        Console.WriteLine("\nВведите количество символов в строке:");
+        count = int.Parse(Console.ReadLine());
+        
+        WriteString(symbol, count);
+        
+        Console.WriteLine("У нас есть вот такой массив случайных чисел:");
+        for (int i = 0; i < myArrey.Length; i++)
+        {
+            Console.Write(myArrey[i] + " ");
+        }
+        Console.WriteLine();
+        
+        
+        Console.WriteLine("Введите число, индекс которого нужно найти в массиве:");
+        numberToFind = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Индекс введенного вами числа в массиве: {FindIndex(myArrey, numberToFind)}");
+    }
+    
+    static void WriteString(char symbol, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(symbol);
+        }
+        Console.WriteLine();
+    }
+    
+    static int FindIndex(int[] myArray, int numberToFind)
+    {
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            if (myArray[i] == numberToFind)
+            {
+                return i; // Возвращаем индекс первого найденного элемента
+            }
+        }
+        return -1; // Если элемент не найден, возвращаем -1
+    }
+    
+    static int[] GetRandomArray(int length, int minValue, int maxValue)
+    {
+        Random random = new Random();
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            array[i] = random.Next(minValue, maxValue);
+        }
+        return array;
+    }
+}
