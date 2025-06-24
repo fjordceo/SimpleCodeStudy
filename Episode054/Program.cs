aced3ac948049ed9dd9712d15960c4973559dde3
fjordceo <fjordceo@hotmail.com>
2025-06-24 20:39:53 +0700

    Lesson 054 completed

--- Episode054/Program.cs
+++ Episode054/Program.cs
@@ -1,9 +1,28 @@
 ﻿namespace Episode054;
+/*
+ *  оператор ?? (null-объединение)
+ *  Синтаксический сахар для проверки на null
+ */
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string name = "Test";
+        //name = null; // Раскомментируй эту строку, чтобы проверить работу оператора ?? (null-объединение)
+
+        // Стандартная проверка на null
+        if (name == null)
+        {
+            Console.WriteLine("Стандартная проверка: Нет данных");
+        }
+        else
+        {
+            Console.WriteLine($"Стандартная провека: {name}");
+        }
+
+        // Синтаксический сахар для проверки на null
+        Console.WriteLine($"Проверка с помощью null объединения: {name ?? "Нет данных"}");
+
     }
 }
