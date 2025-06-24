2a342a084e74543cb0350224cda61c9b7cf05556
fjordceo <fjordceo@hotmail.com>
2025-06-24 20:11:20 +0700

    Lesson 053 completed

---
+++ Episode053/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Episode053;
+
+/*
+ * Что такое null
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Запусти дебагер и посмотри на значения переменных. И все станет понятно.
+        
+        bool b;
+
+        int a;
+
+        double d;
+        
+        Random random;
+
+        int[] myArray;
+        // Создаем переменную, которая будет ссылаться на массив из 10 элементов типа int
+        myArray = new int[10];
+        // Разрываем ссылку на массив. Память, выделенная под массив, будет освобождена сборщиком мусора, когда посчитает нужным.
+        myArray = null;
+        
+        MyClass myClass;
+        
+        MyStruct myStruct;
+    }
+
+    class MyClass
+    {
+        
+    }
+    
+    struct MyStruct
+    {
+        
+    }
+}
---
+++ Episode054/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Episode054;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
