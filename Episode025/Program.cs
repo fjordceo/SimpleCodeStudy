1c8e3367bb3b7a320162a841e397ac75fa25e3a8
fjordceo <fjordceo@hotmail.com>
2025-06-25 11:31:05 +0700

    Lesson 056 is completed

--- Episode025/Program.cs
+++ Episode025/Program.cs
@@ -10,7 +10,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Введите количество циклов");
-        int limit = int.Parse(Console.ReadLine());
+        int limit = int.Parse(Console.ReadLine() ?? string.Empty);
         
         for (int i = 0; i <= limit; i++) // переменная i существует только в рамках данного цикла
         {
---
+++ Episode056/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Episode056;
+
+/*
+ * Оператор условного Null (?.)
+ * Проверяет, не является ли объект null, и если это так, то возвращает null, иначе выполняет доступ к члену объекта.
+ */
+
+class Program
+{
+    static int[] GetArray()
+    {
+        int[] myArray = null;
+        return myArray;
+    }
+    
+    static void Main(string[] args)
+    {
+        int [] myArray = GetArray();
+        Console.WriteLine($"Сумма элементов массива: {myArray?.Sum()?? 0}");
+    }
+}
