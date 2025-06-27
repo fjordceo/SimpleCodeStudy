8ca8f4b243912e0d4612b1dc05430aacdaa157f6
Mikhail Lisaev <fjordceo@hotmail.com>
2025-06-27 21:53:28 +0700

    Lesson 062 is completed

---
+++ Episode063/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Episode063
+    /*
+     * Ключевое слово params
+     */
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
