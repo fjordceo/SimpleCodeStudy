c5117f78e106379c966154632bd2a90fefcb7837
Mikhail Lisaev <fjordceo@hotmail.com>
2025-07-01 14:04:32 +0700

    Lesson 070 is completed

---
+++ Episode070/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Episode070
+/*
+ * Явное и неявное приведение (преобразование) типов 
+ * сужающее и расширяющее преобразование типов
+ */
+
+
+
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double d = a; // Неявное приведение int к double (implicit conversion)
+            float b = a; // Неявное приведение int к float (implicit conversion) (расширяющее преобразование типов)
+            b = (float)d; // Явное приведение double к float (explicit conversion) (сужающее преобразование типов)
+
+            Foo(a); // Неявное приведение int к float
+            Foo((float)d); // Явное приведение double к float
+
+            int firstValue = 5;
+            float secondValue = 10.5f;
+            int result = firstValue + (int)secondValue; 
+            float resultFloat = firstValue + secondValue; 
+            Console.WriteLine(result);
+            Console.WriteLine(resultFloat);
+
+
+
+
+        }
+
+        static void Foo (float value)
+        {
+            Console.WriteLine(value);
+        }
+
+
+
+    }
+}
---
+++ HomeWork011/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace HomeWork011
+
+// Реализовать вывод массива с помошью рекурсии
+// Найти суммму элементов массива с помошью рекурсии
+// Найти сумму цифр числа с помошью рекурсии
+
+
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Основные переменные
+            int[] array = { 1, 2, 3, 4, 5 };
+            int valueForSum = 125678;
+            
+
+            // Вывод элементов массива на консоль
+            Console.Write($"Элеметы массива: ");
+            PrintArray(array);
+            Console.WriteLine();
+
+            // Вывод суммы элементов массива на консоль
+            Console.Write($"Сумма элементов массива: ");
+            Console.WriteLine(SumArray(array));
+
+            // Вывод суммы цифр числа на консоль
+            Console.Write($"Сумма цифр числа {valueForSum}: ");
+            Console.WriteLine(SumDigits(valueForSum));
+
+        }
+        // Метод для вывода массива на консоль с помошью рекурсии
+        static void PrintArray(int[] array, int index = 0)
+        {
+            if (index < array.Length)
+            {
+                Console.Write(array[index] + " ");
+                PrintArray(array, index + 1);
+            }
+        }
+
+        // Метод для нахождения суммы элементов массива с помошью рекурсии
+        static int SumArray(int[] array, int index = 0)
+        {
+            if (index >= array.Length)
+            {
+                return 0;
+            }    
+            
+            return array[index] + SumArray(array, index + 1);
+        }
+
+        // Метод для нахождения суммы цифр числа с помошью рекурсии
+        static int SumDigits(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            return number % 10 + SumDigits(number / 10);
+        }
+    }
+}
