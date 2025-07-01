950170ca3f5dd5f0a6c6018099c8142bfb1d6cca
Mikhail Lisaev <fjordceo@hotmail.com>
2025-07-01 15:39:30 +0700

    Lesson 072 is completed

---
+++ Episode071/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Episode071
+/*
+ * "Ядерный Ганди" и арифметическое переполнение
+ */
+
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte aggression = 1;
+
+            byte democracyModifier = 2;
+
+            aggression = (byte)(aggression - democracyModifier);
+            //aggression = checked((byte)(aggression - democracyModifier));  - Проверка на переполнение
+            // checked
+            // {}
+            // unchecked - отключение проверки на переполнение, работает если в проекте включена проверка на переполнение
+
+
+            Console.WriteLine($"Агрессивность Ганди: {aggression}");
+
+
+            int a = int.MaxValue; // 2147483647
+            a = a + 1; // Переполнение через верхнюю границу
+
+            int b = int.MinValue; // -2147483648
+            b = b - 1; // Переполнение через нижнюю границу
+
+            Console.WriteLine($"Значение А: {a}, значение B: {b}");
+
+        }
+    }
+}
---
+++ Episode072/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+
+namespace Episode072
+    /*
+     * Null-совместимые значимые типы (Nullable types)
+     */
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ссылочные типы могут быть null
+            string str = null; // Строка может быть null
+
+
+            // Значимые типы не могут быть null, но можно использовать Nullable<T> или ? для создания null-совместимых типов
+           DateTime? date = null; // Nullable<DateTime> или DateTime? позволяет хранить null
+
+            int? a = null; // Ошибка компиляции, int не может быть null
+
+            Console.WriteLine(a == null); // Проверка на null для Nullable<int>
+
+            Console.WriteLine(a.HasValue); // Проверка на наличие значения в Nullable<int>
+
+            Console.WriteLine(a.GetValueOrDefault()); // Получение значения или 0, если значение null
+
+            Console.WriteLine(a.GetValueOrDefault(3)); // Получение значения или 3, если значение null
+
+            Console.WriteLine(a ?? 55); //  Использование оператора ?? для получения значения или 55, если значение null
+
+            //Console.WriteLine(a.Value); // Получение значения, если значение не null, иначе выбросит исключение InvalidOperationException
+
+
+
+
+
+
+        }
+    }
+}
