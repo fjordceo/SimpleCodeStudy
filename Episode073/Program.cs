313f9c0baf5d0afa501afd4b583f9c324eb029be
Mikhail Lisaev <fjordceo@hotmail.com>
2025-07-01 17:48:02 +0700

    Lesson 074 is completed

---
+++ Episode073/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Episode073
+/*
+ * Что такое var
+ * Неявно типизированные локальные  переменные
+ */
+
+
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = 10;
+            var b = "Hello, World!";
+            var a = new float[] { 1.0f, 2.0f, 3.0f };
+
+            var i = new Dictionary<int, string>
+            {
+                { 1, "One" },
+                { 2, "Two" },
+                { 3, "Three" }
+            };
+
+            // Dictionary<int, string> i = new Dictionary<int, string> - пример явного указания типа
+
+            // Работа с анонимными типами
+
+
+
+
+
+            Console.WriteLine(t.GetType());
+            Console.WriteLine(b.GetType());
+            Console.WriteLine(a.GetType());
+        }
+    }
+}
---
+++ Episode074/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Episode074
+/*
+ * enum (перечисление)
+ */
+
+{
+    internal class Program
+    { 
+        enum DayOfWeek : byte // Можно использовать byte, sbyte, short, ushort, int, uint, long, ulong
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            // Пример использования перечисления
+            //while (true)
+            //{
+            //    ConsoleKey key = Console.ReadKey(true).Key;
+
+            //    int keyValue = (int)key;
+
+            //    Console.WriteLine($"\tEnum {key}\tKey Code: {keyValue}");
+
+            //    if (key == ConsoleKey.Enter)
+            //    {
+            //        Console.WriteLine("Вы нажали Enter!");
+
+            //    }
+            //}
+
+            DayOfWeek dayOfWeek = DayOfWeek.Monday;
+
+            Console.WriteLine(Enum.GetUnderlyingType(typeof(DayOfWeek)));
+            Console.WriteLine(dayOfWeek);
+            Console.WriteLine((int)dayOfWeek);
+            Console.WriteLine((DayOfWeek)3); // Приведение к перечислению
+            
+          
+
+
+
+        }
+    }
+}
