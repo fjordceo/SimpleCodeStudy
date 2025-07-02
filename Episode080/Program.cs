bab299dde783da53aed7feea9264a40df2b017ea
Mikhail Lisaev <fjordceo@hotmail.com>
2025-07-02 14:21:10 +0700

    Lesson 080 is completed

---
+++ Episode080/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Episode080
+/*
+ * Перезагрузка конструктора - это возможность создавать несколько конструкторов в одном классе с разными параметрами.
+ */
+{
+    class Point
+    {
+        public Point()
+        {
+
+        }
+
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;     
+        }
+
+        public Point(int x, int y, int z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        private int _x;
+        private int _y;
+        private int _z;
+
+        public void Print()
+        {
+            Console.WriteLine($"Point: ({_x}, {_y})");
+        }
+    }
+        
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+        }
+    }
+}
