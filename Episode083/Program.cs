6ab28497129d32fba07ca3ce16cf1824d0a52f8b
Mikhail Lisaev <fjordceo@hotmail.com>
2025-07-02 16:10:50 +0700

    Lesson 083 is completed

---
+++ Episode083/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Episode083
+/*
+ * Ключевое слово static в C# используется для объявления статических членов класса.
+ * 
+ * Статистические поля класса принадлежат самому классу, а не его экземплярам.
+ */
+
+{
+    class MyClass
+    {
+        public int a;
+        private static int b; // Статическое поле класса
+        public void SetB(int b)
+        {
+            MyClass.b = b;
+        }
+        public void PrintB()
+        {
+            Console.WriteLine(b);
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            MyClass myClass1 = new MyClass();
+            myClass1.a = 10; // Установка значения поля a для первого экземпляра
+            myClass1.SetB(100);
+            
+            MyClass myClass2 = new MyClass();
+            myClass2.a = 20; // Установка значения поля a для второго экземпляра
+            myClass2.PrintB();
+        }
+    }
+}
